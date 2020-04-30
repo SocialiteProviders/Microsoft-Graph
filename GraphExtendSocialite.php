ebdf35711ff3e289cef2dbae847af703a4e7d6a8
Jimbolino <Jimbolino@users.noreply.github.com>
2020-05-01 01:01:40 +0200

    Fix Class does not comply with psr-4 autoloading standard (#403)

---
+++ GraphExtendSocialite.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SocialiteProviders\Graph;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class GraphExtendSocialite
+{
+    /**
+     * Execute the provider.
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite(
+            'graph',
+            __NAMESPACE__.'\Provider'
+        );
+    }
+}
